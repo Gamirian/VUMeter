aca8c35b40c5bacdf6e001f0edc19848fa0d831a
Gamirian <156970049+Gamirian@users.noreply.github.com>
2024-04-22 20:44:08 -0700

    Added Submission and Date

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,14 @@
 # CHANGELOG
 
-## {DATE}
+## April 22nd, 2024
 ### Contributors
-...
+Garno Amirian
 
 ### Added
-...
+Submission and Code
 
 ### Changed
-...
+N/A
 
 ### Removed
-...
+N/A
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from digitalio import DigitalInOut, Direction
 from analogio import AnalogIn
 from time import sleep
 
-# setup pins
+#setup pins
 microphone = AnalogIn(board.IO1)
 
 status = DigitalInOut(board.IO17)
@@ -11,9 +11,16 @@ status.direction = Direction.OUTPUT
 
 led_pins = [
     board.IO21,
-    board.IO26, # type: ignore
+    board.IO26,  # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33,
+    board.IO34,
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -21,17 +28,14 @@ leds = [DigitalInOut(pin) for pin in led_pins]
 for led in leds:
     led.direction = Direction.OUTPUT
 
-# main loop
+def update_leds(volume, num_leds):
+    light_intensity = (volume-20000)/1750
+    print(light_intensity)
+    for i, led in enumerate(leds):
+        led.value = i<light_intensity
+
 while True:
     volume = microphone.value
-
     print(volume)
-
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
-
-    sleep(1)
-
-    # instead of blinking,
-    # how can you make the LEDs
-    # turn on like a volume meter?
+    update_leds(volume, len(leds))
+    sleep(0.1)
